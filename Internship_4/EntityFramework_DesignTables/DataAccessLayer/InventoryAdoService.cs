997ddbcf619973792962f4ccdaff25c5fe729a05
Emma Naghdalyan <emma.naghdalyan@gmail.com>
2021-07-08 16:55:53 +0400

    Ado Services commit

---
+++ Internship_4/EntityFramework_DesignTables/DataAccessLayer/InventoryAdoService.cs
@@ -0,0 +1,81 @@
+﻿using EntityFramework_DesignTables.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework_DesignTables.DataAccessLayer
+{
+    public class InventoryAdoService
+    {
+        IConfigurationRoot configuration;
+        string connectionString;
+
+        public InventoryAdoService()
+        {
+            configuration = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json")
+                                .Build();
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<List<Inventory>> GetInventoriesAsync()
+        {
+            string command = $"SELECT * FROM Inventories;";
+            List<Inventory> inventories = new List<Inventory>();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                sqlConnection.Open();
+                var reader = await sqlCommand.ExecuteReaderAsync();
+                while(reader.Read())
+                {
+                    inventories.Add(new Inventory
+                    {
+                        InventoryId = reader.GetInt32(0),
+                        Type = reader.GetString(1),
+                        SerialNumber = reader.GetString(2)
+                    });
+                }
+            }
+            return inventories;
+        }
+
+        public async Task CreateInventoryAsync(Inventory inventory)
+        {
+            string command = $"INSERT INTO Inventories(Type, SerialNumber) VALUES({inventory.Type},{inventory.SerialNumber});";
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                sqlConnection.Open();
+                await sqlCommand.ExecuteNonQueryAsync();
+            }
+        }
+
+        public async Task UpdateInventoryAsync(Inventory inventory)
+        {
+            string command = $"UPDATE Inventories SET Type={inventory.Type}, SerialNumber={inventory.SerialNumber} WHERE InventoryId={inventory.InventoryId};";
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                sqlConnection.Open();
+                await sqlCommand.ExecuteNonQueryAsync();
+            }
+        }
+
+        public async Task DeleteInventoryAsync(int id)
+        {
+            string command = $"DELETE FROM Inventories WHERE InventoryId={id};";
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                sqlConnection.Open();
+                await sqlCommand.ExecuteNonQueryAsync();
+            }
+        }
+    }
+}
---
+++ Internship_4/EntityFramework_DesignTables/DataAccessLayer/OrderAdoService.cs
@@ -0,0 +1,81 @@
+﻿using EntityFramework_DesignTables.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFramework_DesignTables.DataAccessLayer
+{
+    public class OrderAdoService
+    {
+        IConfigurationRoot configuration;
+        string connectionString;
+
+        public OrderAdoService()
+        {
+            configuration = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json")
+                                .Build();
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<List<Order>> GetOrdersAsync()
+        {
+            string command = $"SELECT * FROM Orders;";
+            List<Order> orders = new List<Order>();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                sqlConnection.Open();
+                var reader = await sqlCommand.ExecuteReaderAsync();
+                while (reader.Read())
+                {
+                    orders.Add(new Order
+                    {
+                        OrderId = reader.GetInt32(0),
+                        CustomerId = reader.GetInt32(1),
+                        TrackingNumber = reader.GetInt32(2)
+                    });
+                }
+            }
+            return orders;
+        }
+
+        public async Task CreateOrderAsync(Order order)
+        {
+            string command = $"INSERT INTO Orders(CustomerId, TrackingNumber) VALUES({order.CustomerId},{order.TrackingNumber});";
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                sqlConnection.Open();
+                await sqlCommand.ExecuteNonQueryAsync();
+            }
+        }
+
+        public async Task UpdateOrderAsync(Order order)
+        {
+            string command = $"UPDATE Orders SET CustomerId={order.CustomerId}, TrackingNumber={order.TrackingNumber} WHERE OrderId={order.OrderId};";
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                sqlConnection.Open();
+                await sqlCommand.ExecuteNonQueryAsync();
+            }
+        }
+
+        public async Task DeleteOrderAsync(int id)
+        {
+            string command = $"DELETE FROM Orders WHERE OrderId={id};";
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                sqlConnection.Open();
+                await sqlCommand.ExecuteNonQueryAsync();
+            }
+        }
+    }
+}
